0112aad82e88d999f0ef270c52efa16e770fb263
vinips1993 <rm550907@fiap.com.br>
2023-08-26 16:29:13 -0300

    Incluindo for

--- main.js
+++ main.js
@@ -4,22 +4,16 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template_String
 
-    //template_String
-    const idAudio = `#som_${instrumento}`;
-    //console.log(instrumento)
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador = contador + 1;
-
-    //console.log(contador);
+    
 }
