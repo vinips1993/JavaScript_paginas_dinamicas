3e28504e47ff0d9ea6b5b6dc1a2573bbc1be2057
vinips1993 <rm550907@fiap.com.br>
2023-08-26 20:38:12 -0300

    Finalizando Eventos e Lógica

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 function tocaSom (seletorAudio) {
    const elemento = document.querySelector(seletorAudio)
 
-    if (elemento === null){
-        //alert('Elemento não encontrado')
-        console.log('Elemento não encontrado!')
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
     }
-
-    if (elemento != null) {
-        if(elemento.localName === 'audio'){
-            elemento.play();
-        }
-    }   
+    else {
+        //alert('Elemento não encontrado')
+        console.log('Elemento não encontrado ou seletor inválido!')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
