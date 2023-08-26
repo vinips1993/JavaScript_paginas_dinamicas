99a8e2a4e9ff735ee9fb89119c584fe8c3c64efc
vinips1993 <rm550907@fiap.com.br>
2023-08-26 20:23:00 -0300

    Incluindo outras condições

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio)
+
+    if (elemento === null){
+        //alert('Elemento não encontrado')
+        console.log('Elemento não encontrado!')
+    }
+
+    if (elemento != null) {
+        if(elemento.localName === 'audio'){
+            elemento.play();
+        }
+    }   
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
