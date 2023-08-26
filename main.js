8bc2a354e2893a69a1fcce1b820f1f782121dfb8
vinips1993 <rm550907@fiap.com.br>
2023-08-26 15:31:12 -0300

    interando_teclas

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom () {
+    document.querySelector().play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1
+
+    console.log(contador);
+}
